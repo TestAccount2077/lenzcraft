68742b7064d8ce19d042c0e29dc53f6aa5e9866b
TestAccount2077 <maselectronics594@gmail.com>
2020-09-27 20:34:06 +0200

    Update

---
+++ lenzcraft/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    url(r'^', include('main.urls')),
+    url(r'^', include('accounts.urls')),
+    url(r'^admin/', admin.site.urls),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
