b66c38b547dde07c06bff08e6bef7efa20e97719
TestAccount2077 <maselectronics594@gmail.com>
2020-09-27 20:46:45 +0200

    Update settings.py

--- lenzcraft/settings.py
+++ lenzcraft/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '1&#q@(l%qw%%hsh(cbq4%ob#x$rn3fhi3llb0o=ka@m7wfj39&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
